ddb7ceef1f367629f834d9cfcfe96e9e18a76ada
2826450853 <55837404+2826450853@users.noreply.github.com>
2021-07-05 20:37:38 +0800

    Add files via upload

--- project/index.js
+++ project/index.js
@@ -1,65 +1,84 @@
-const RootComponent = {
-    data () {
-        return {
-            ClassName1:"div1",
-            ClassName2:"div2",
-            ClassName3:"div3"
-        }
-    },
-    methods: {
-      onmouseenter1(){
-        this.ClassName1="div1_hover";
-        setTimeout(() =>{
-          this.ClassName1="div1";
-      },300);
-      },
-      onmouseenter2(){
-        this.ClassName2="div2_hover";
-        setTimeout(() =>{
-          this.ClassName2="div2";
-      },300);
-      },
-      onmouseenter3(){
-        this.ClassName3="div3_hover";
-        setTimeout(() =>{
-          this.ClassName3="div3";
-      },300);
+const Menu1 = {
+  data () {
+      return {
+          c1:false,
+          c2:false,
+          c3:false,
+          seen1:false,
+          seen2:false,
+          seen2:false,
+          f1:false,
+          f2:false,
+          f3:false,
+          r1:true,
+          bf1:false,
+          sf1:false,//书签是否显示
+          sb1:false,//书签跳跃动画
+          sf2:true,//书签出场动画
       }
-    }
+  },
+  methods: {
+    onmouseenter1(){
+      this.c1=true;
+      setTimeout(() =>{
+        this.c1=false;
+    },300);
+    },
+    onmouseenter2(){
+      this.c2=true;
+      setTimeout(() =>{
+        this.c2=false;
+    },300);
+    },
+    onmouseenter3(){
+      this.c3=true;
+      setTimeout(() =>{
+        this.c3=false;
+    },300);
+    },
+    onmouseenter4(){
+      this.bf1=true;
+      setTimeout(() =>{
+        this.bf1=false;
+      },1000);
+    },
+    onmouseenter5(){
+      this.sb1=true;
+      setTimeout(() =>{
+        this.sb1=false;
+        this.sf2=false;
+      },1000);
+    },
+    show(){
 
+      setTimeout(() =>{
+        this.seen1=true;
+        this.f1=true;
+    },250);
+      setTimeout(() =>{
+        this.seen2=true;
+        this.f2=true;
+    },500);
+      setTimeout(() =>{
+        this.seen3=true;
+        this.f3=true;
+    },750);
+      setTimeout(() =>{
+        this.f1=false;
+    },1250);
+    setTimeout(() =>{
+      this.f2=false;
+    },1550);
+    setTimeout(() =>{
+      this.f3=false;
+    },2000);
+    setTimeout(() =>{
+      this.r1=false;
+    },2000);
+    setTimeout(() =>{
+      this.sf1=true;
+    },1000);
+    },
+  }
 }
-const app = Vue.createApp(RootComponent);
-const vm = app.mount('#header_div');
-
-// const Menu1 = {
-//   data() {
-//     return {
-//       seen:true,
-//       f1:false,
-//       f2:false,
-//       f3:false,
-//       f4:false,
-//     }
-//   },
-//   methods: {
-//     show(){
-//       setTimeout(() =>{
-//         this.f1=true;
-//     },250);
-//       setTimeout(() =>{
-//         this.f2=true;
-//     },500);
-//       setTimeout(() =>{
-//         this.f3=true;
-//     },750);
-//     setTimeout(() =>{
-//       this.f4=true;
-//     },1000);
-//     },
-//   }
-
-// }
-// const vm1 = Vue.createApp(Menu1).mount('#accordion1');
-// // vm1.show();
-
-
+Vue.createApp(Menu1).mount('#header_div').show();
